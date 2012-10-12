0059857a401d578757ae48eec712db1690ddda65
Anup <anup@kunalinfotech.net>
2012-10-12 15:29:11 +0530

    production host url changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,5 +67,5 @@ DemoCloud::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
- config.action_mailer.default_url_options = { :host => "http://codersave.herokuapp.com" }
+ config.action_mailer.default_url_options = { :host => "codersave.herokuapp.com" }
 end
