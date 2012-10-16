07d1739880fc65af4ea87e367ca54b80ad1e583d
Anup <anup@kunalinfotech.net>
2012-10-16 12:59:28 +0530

    omniauth changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Adding the facebook key for Omniauth.
 
-  provider :facebook, '407701972628459', 'baa6c5809a01fa35c4c01e11eae0cd44', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status, offline_access'}
+  provider :facebook, '407701972628459', 'baa6c5809a01fa35c4c01e11eae0cd44', {:scope => 'email, read_stream, read_friendlists, friends_likes, friends_status'}
 end
 
   
