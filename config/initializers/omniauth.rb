ab96445a64d827a99eecece470cc5248e9a1ea77
Anup <anup@kunalinfotech.net>
2012-10-17 17:26:46 +0530

    read stream removed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # Adding the facebook key for Omniauth.
 
-  provider :facebook, '407701972628459', 'baa6c5809a01fa35c4c01e11eae0cd44'
+  provider :facebook, '407701972628459', 'baa6c5809a01fa35c4c01e11eae0cd44', {:scope => 'email, offline_access'}
 end
 
   
