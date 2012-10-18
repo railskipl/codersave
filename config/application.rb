c34bd9c6082a71118d76724cb5d30f1ffd60cae5
Anup <anup@kunalinfotech.net>
2012-10-18 12:00:49 +0530

    devise initializers changes

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,9 @@ module DemoCloud
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    env['devise.mapping'] = Devise.mappings[:user]
+    
 
     # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
