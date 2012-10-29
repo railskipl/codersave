e0e6dada6a3a3b7733a26d49f78682517ddfa39a
Anup <anup@kunalinfotech.net>
2012-10-29 15:22:44 +0530

    carrierwave initializers file added

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -15,6 +15,7 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+  
   def cache_dir
       "#{Rails.root}/tmp/uploads"
   	end
